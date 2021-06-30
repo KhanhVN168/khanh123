745b75fe150d5927cb804702f1e319536dd662b4
= <=>
2021-06-30 18:58:47 +0700

    a

---
+++ src/Lab12_4/Lab12_4Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab12_4;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import static javafx.application.Application.launch;
+
+/**
+ *
+ * @author icom
+ */
+public class Lab12_4Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        GridPane root = FXMLLoader.load(getClass().getResource("Lab12_4.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Registration");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/lab12_3/Lab12_3Main.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab12_3;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import static javafx.application.Application.launch;
+
+/**
+ *
+ * @author icom
+ */
+public class Lab12_3Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        GridPane root = FXMLLoader.load(getClass().getResource("Lab12_3.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Campsite");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
